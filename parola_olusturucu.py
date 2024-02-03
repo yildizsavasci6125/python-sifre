ab978318209f4f0a43aaa7c1bcf12da8bcfcc1cf
yildizsavasci6125 <158583542+yildizsavasci6125@users.noreply.github.com>
2024-02-03 20:42:39 +0300

    Create parola_olusturucu.py

---
+++ parola_olusturucu.py
@@ -0,0 +1,8 @@
+import random
+karakter = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+uzunluk = int(input ("Parolanın uzunluğunu giriniz."))
+yeni_sifre= "5"
+for i in range(uzunluk):
+    yeni_sifre+=random.choice(karakter)
+print("Sizin için uygun şifre" , *yeni_sifre)    
+    
